97643daa75aa9260847106da1b8469e4fcc17410
Dominic Sauer <dominic.sauer@yahoo.de>
2016-12-06 16:01:54 +0100

    Add lexical and syntactic writing style features

--- core/feature_extraction/writing_style.py
+++ core/feature_extraction/writing_style.py
@@ -5,6 +5,7 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 
 class WritingStyleFeatures:
     special_characters = '~ @#$%^&*-_= ,+><[]{}/\|'
+    punctuations = ',.?!:;`"'
 
     def __init__(self, text):
         self.text = text
@@ -24,8 +25,10 @@ class WritingStyleFeatures:
                                  self.avg_sentence_length_chars(),
                                  self.avg_sentence_length_words(),
                                  self.number_of_unique_word()]
-        structural_features = [self.number_of_sentences()]
-        lexical_features = lexical_character_features + lexical_word_features + structural_features
+        syntactic_features = self.char_frequencies(self.punctuations) + self.function_word_frequency()
+        structural_features = [self.number_of_lines(),
+                               self.number_of_sentences()]
+        lexical_features = lexical_character_features + lexical_word_features + syntactic_features + structural_features
 
         return lexical_features
 
@@ -58,3 +61,23 @@ class WritingStyleFeatures:
 
     def avg_sentence_length_words(self):
         return np.mean([len(word_tokenize(s)) for s in sent_tokenize(self.text, language='english')])
+
+    def number_of_lines(self):
+        return len(self.text.splitlines())
+
+    def function_word_frequency(self):
+        function_words = ['a', 'between', 'in', 'nor', 'some', 'upon', 'about', 'both', 'including', 'nothing',
+                          'somebody us', 'above', 'but', 'inside', 'of', 'someone', 'used', 'after', 'by', 'into',
+                          'off', 'something', 'via', 'all', 'can', 'is', 'on', 'such', 'we', 'although', 'cos', 'it',
+                          'once', 'than', 'what', 'am', 'do', 'its', 'one', 'that', 'whatever', 'among', 'down',
+                          'latter', 'onto', 'the', 'when', 'an', 'each', 'less', 'opposite', 'their', 'where', 'and',
+                          'either', 'like', 'or', 'them', 'whether', 'another', 'enough', 'little', 'our', 'these',
+                          'which', 'any', 'every', 'lots', 'outside', 'they', 'while', 'anybody', 'everybody', 'many',
+                          'over', 'this', 'who', 'anyone', 'everyone', 'me', 'own', 'those', 'whoever', 'anything',
+                          'everything', 'more', 'past', 'though', 'whom', 'are', 'few', 'most', 'per', 'through',
+                          'whose', 'around', 'following', 'much', 'plenty', 'till', 'will', 'as', 'for', 'must', 'plus',
+                          'to', 'with', 'at', 'from', 'my', 'regarding', 'toward', 'within', 'be', 'have', 'near',
+                          'same', 'towards', 'without', 'because', 'he', 'need', 'several', 'under', 'worth', 'before',
+                          'her', 'neither', 'she', 'unless', 'would', 'behind', 'him', 'no', 'should', 'unlike', 'yes',
+                          'below', 'i', 'nobody', 'since', 'until', 'you', 'beside', 'if', 'none', 'so', 'up', 'your']
+        return [word_tokenize(self.text).count(w) for w in function_words]

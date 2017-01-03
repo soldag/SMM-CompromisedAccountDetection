6e7154d3abbc2f13c0d32e6525c04af39972c9d5
Sören Oldag <soeren_oldag@freenet.de>
2017-01-03 20:10:47 +0100

    Fix window implementation.

--- core/__init__.py
+++ core/__init__.py
@@ -1,12 +1,9 @@
-import random
-
 from .data_provider import get_status_updates
 from .feature_extraction import extract_features, extract_features_batch
 from .training import train_classifier, create_classifier
-from .evaluation import evaluate
 
 
-START_BATCH_SIZE = 50
+START_BATCH_SIZE = 100
 
 
 def analyze_status_updates(user_status_updates, ext_status_updates,
@@ -21,8 +18,8 @@ def analyze_status_updates(user_status_updates, ext_status_updates,
     # Train classifier iteratively
     start = 0
     end = START_BATCH_SIZE
+    suspected_status_updates = []
     classifier = create_classifier(classifier_type)
-    false_predictions = []
     while end < len(user_status_updates):
         # Train model with status updates in window (start to end)
         print("Train model with window size %s to %s" % (start, end))
@@ -33,22 +30,36 @@ def analyze_status_updates(user_status_updates, ext_status_updates,
             classifier.train_iteratively(user_status_updates[start:end], [])
 
         # Predict for remaining status updates
-        # TODO shuffle with external tweets for evaluation
         predictions = classifier.predict(user_status_updates[end:])
 
         # Extend status update window by safe zone (first x status updates
         # that were classified as written by the user)
-        safe_zone_length = len(predictions)
-        if False in predictions:
-            false_predictions.append(user_status_updates[start + predictions.index(False)])
-            user_status_updates.remove(user_status_updates[start + predictions.index(False)])
+        safe_zone_length = _index(predictions, False, len(predictions))
 
-            # Move window to start after the false prediction
-            start = start + predictions.index(False) + 1
+        # Move window
+        if safe_zone_length == 0:
+            num_false_predictions = _index(predictions, True, len(predictions))
+            suspected_status_updates += [user_status_updates[end + i]
+                                         for i in range(num_false_predictions)]
+            start = end + num_false_predictions
+            end = start + _index(predictions[:num_false_predictions], False,
+                                 len(predictions) - num_false_predictions - 1) + 1
         else:
-            # Move window
             start = end
+            end = start + safe_zone_length
 
-        end = start + safe_zone_length
+        # Predict suspected status updates again with new model
+        if suspected_status_updates:
+            predictions = classifier.predict(suspected_status_updates)
+            suspected_status_updates = [suspected_status_updates[i]
+                                        for i in range(len(suspected_status_updates))
+                                        if not predictions[i]]
 
-    return false_predictions
+    return suspected_status_updates
+
+
+def _index(iterable, value, default_value=None):
+    if value in iterable:
+        return iterable.index(value)
+
+    return default_value

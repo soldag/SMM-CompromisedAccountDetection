98727e374f4c6fe16ed7d31f7b52527728098ff3
Dominic Sauer <dominic.sauer@yahoo.de>
2017-01-03 11:17:00 +0100

    Adapt app in order to use sliding window approach

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
+from core.data_provider import get_status_updates
 from core import prepare_data
-from core import run_pipeline
+from core import analyze_status_updates
 
 import urllib.parse as url_parser
 
@@ -16,8 +17,9 @@ def check():
         url = request.form.get('account_url', 'hpi_de')
         parsed_url = url_parser.urlparse(url)
         user_id = parsed_url.path.split('/')[1]
-        status_updates = prepare_data('twitter', user_id=user_id)
-        tp, tn, fp, fn = run_pipeline(status_updates, 'decision_tree')
-        return 'tp: ' + str(tp) + ', tn: ' + str(tn) + ', fp: ' + str(fp) + ', tn: ' + str(tn)
+        user_status_updates = get_status_updates('twitter', user_id=user_id)
+        ext_status_updates = prepare_data('fth', dataset_path="./follow_the_hashtag_usa.csv")
+        result = analyze_status_updates(user_status_updates, ext_status_updates, 'perceptron')
+        return str(result)
     else:
         return render_template('check.html')

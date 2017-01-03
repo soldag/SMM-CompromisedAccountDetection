0bb7d4208ab4ef2cd67c0c50ab07853388c182be
Dominic Sauer <dominic.sauer@yahoo.de>
2017-01-03 14:47:58 +0100

    Clean up core: remove run_pipeline() and prepare_data() functions, call get_status_updates in app directly on fth data provider

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import request
 from flask import render_template
 
 from core.data_provider import get_status_updates
-from core import prepare_data
 from core import analyze_status_updates
 
 import urllib.parse as url_parser
@@ -18,7 +17,7 @@ def check():
         parsed_url = url_parser.urlparse(url)
         user_id = parsed_url.path.split('/')[1]
         user_status_updates = get_status_updates('twitter', user_id=user_id)
-        ext_status_updates = prepare_data('fth', dataset_path="./follow_the_hashtag_usa.csv")
+        ext_status_updates = get_status_updates('fth', dataset_path="./data/follow_the_hashtag_usa.csv")
         result = analyze_status_updates(user_status_updates, ext_status_updates, 'perceptron')
         if result:
             return render_template('check_success.html')

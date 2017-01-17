42dc56ee16da11425902d9e2b15e8bf92c1aa31d
Dominic Sauer <dominic.sauer@yahoo.de>
2017-01-17 16:04:51 +0100

    Add ngram feature extraction - not optimized

--- core/feature_extraction/__init__.py
+++ core/feature_extraction/__init__.py
@@ -2,18 +2,21 @@ from sklearn import preprocessing
 
 from .writing_style import WritingStyleFeatures
 from .meta_data import MetaDataFeatures
+from .n_grams import NGramFeatures, extract_n_grams
 
 
-def extract_features(resource):
+def extract_features(resource, grams):
     writing_style_features = WritingStyleFeatures(resource.content).get_features()
     meta_data_features = MetaDataFeatures(resource).get_features()
-    features = writing_style_features + meta_data_features
+    n_gram_features = NGramFeatures(resource.content, grams).get_features()
+    features = writing_style_features + meta_data_features + n_gram_features
 
     return list(map(float, features))
 
 
 def extract_features_batch(resources, scale=True):
-    features = [extract_features(resource) for resource in resources]
+    grams = extract_n_grams(resources)
+    features = [extract_features(resource, grams) for resource in resources]
     if scale and resources:
         features = preprocessing.scale(features).tolist()
 
---
+++ core/feature_extraction/n_grams.py
@@ -0,0 +1,43 @@
+from nltk.util import ngrams
+from nltk import word_tokenize
+from collections import Counter
+
+
+def extract_n_grams(resources):
+    c_grams = []
+    w_grams = []
+    for resource in resources:
+        c_grams += NGramFeatures(resource.content).get_character_grams()
+        w_grams += NGramFeatures(resource.content).get_word_grams()
+    c_grams_count = Counter(c_grams).most_common(25)    #later on maybe where count > 5
+    w_grams_count = Counter(w_grams).most_common(50)
+    c_grams = [gram_count[0] for gram_count in c_grams_count]
+    w_grams = [gram_count[0] for gram_count in w_grams_count]
+    return [c_grams, w_grams]
+
+
+class NGramFeatures:
+    N = 4
+
+    def __init__(self, text, grams=None):
+        self.text = text
+        self.grams = grams
+
+    def get_features(self):
+        return self.character_n_gram_feature() + self.word_n_gram_feature()
+
+    def character_n_gram_feature(self):
+        grams_count = Counter(elem for elem in self.get_character_grams())
+
+        return [grams_count[c_gram] for c_gram in self.grams[0]]
+
+    def word_n_gram_feature(self):
+        grams_count = Counter(elem for elem in self.get_word_grams())
+
+        return [grams_count[w_gram] for w_gram in self.grams[1]]
+
+    def get_character_grams(self):
+        return [x for x in ngrams(self.text, self.N)]
+
+    def get_word_grams(self):
+        return [x for x in ngrams(word_tokenize(self.text), self.N)]

412af5402df60fd806d5fe0305b2cba44d75929c
Dominic Sauer <dominic.sauer@yahoo.de>
2017-02-22 15:54:50 +0100

    Add decision tree classifier parameters to classifier analyzer

--- core/utils/classifier_optimizer.py
+++ core/utils/classifier_optimizer.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-from time import time
+from time import time, ctime
 from scipy.stats import randint as sp_randint
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.feature_selection import VarianceThreshold
-from sklearn import linear_model
+from sklearn import linear_model, tree
 
 from core.feature_extraction import extract_features_batch
 
@@ -25,16 +25,19 @@ def prepare_two_class_data(user_statuses, ext_statuses):
 
 # Utility function to report best scores
 def report(results, n_top=3):
-    file = open('test.txt', 'a')
+    file = open('classifier_evaluation_report.log', 'a')
+    file.write("{0}:\n".format(
+        ctime(int(time()))))
     for i in range(1, n_top + 1):
         candidates = np.flatnonzero(results['rank_test_score'] == i)
         for candidate in candidates:
-            file.write("Model with rank: {0}".format(i))
-            file.write("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+            file.write("Model with rank: {0} # ".format(i))
+            file.write("Mean validation score: {0:.3f} (std: {1:.3f}) # ".format(
                 results['mean_test_score'][candidate],
                 results['std_test_score'][candidate]))
             file.write("Parameters: {0}".format(results['params'][candidate]))
             file.write("\n")
+    file.write("\n")
     file.close()
 
 
@@ -60,9 +63,9 @@ CLASSIFIER_MAPPING = {
         },
         'grid': {
             'penalty': [None, 'l2', 'l1', 'elasticnet'],
-            'alpha': [0.0001, 0.001, 0.01, 0.1, 1],
+            'alpha': [0.0001, 0.0003, 0.0004, 0.0005, 0.0006, 0.0007, 0.0008, 0.0009],
             'fit_intercept': [True, False],
-            'n_iter': [1, 10, 20],
+            'n_iter': [5, 10, 20, 30],
             'shuffle': [True, False],
             'verbose': [0, 2, 4],
             'eta0': [1.0, 1.5, 2.0],
@@ -71,9 +74,34 @@ CLASSIFIER_MAPPING = {
             'warm_start':  [True, False]
         }
     },
-    'decision_tree': {},
-    'one_class_svm': {},
-    'isolation_forest': {}
+    'decision_tree': {
+        'clf': tree.DecisionTreeClassifier,
+        'prep': prepare_two_class_data,
+        'dist': {
+            'criterion': ['gini', 'entropy'],
+            'splitter': ['best', 'random'],
+            'max_features': [None, 'auto', 'sqrt', 'log2'],
+            'max_depth': sp_randint(1, 1000),
+            'min_samples_split': sp_randint(1, 1000),
+            'min_samples_leaf': [1, 4, 0.2, 0.5],
+            'min_weight_fraction_leaf': [0.0, 0.5],
+            'max_leaf_nodes': [None, 10, 20],
+            'class_weight': [None, 'balanced'],
+            'presort': [False, True]
+        },
+        'grid': {
+            'criterion': ['gini', 'entropy'],
+            'splitter': ['best', 'random'],
+            'max_features': [None, 'auto', 'sqrt', 'log2'],
+            'max_depth': [None, 1, 10, 100, 500, 1000],
+            'min_samples_split': [2, 100, 500, 1000],
+            'min_samples_leaf': [1, 100, 0.2, 0.5],
+            'min_weight_fraction_leaf': [0.0, 0.3, 0.5],
+            'max_leaf_nodes': [None, 10, 100, 500],
+            'class_weight': [None, 'balanced'],
+            'presort': [False, True]
+        }
+    }
 }
 
 
@@ -100,7 +128,6 @@ class ClassifierOptimizer:
         n_iter_search = 20
         random_search = RandomizedSearchCV(self.classifier, param_distributions=self.param_dist,
                                            n_iter=n_iter_search)
-
         start = time()
         random_search.fit(self.data, self.target)
         print("RandomizedSearchCV took %.2f seconds for %d candidates"

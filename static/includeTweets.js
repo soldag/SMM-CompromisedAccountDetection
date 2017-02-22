5a0c75f791d3900c629bcd797ce8f21e15ec8db5
Sören Oldag <soeren_oldag@freenet.de>
2017-02-22 23:40:01 +0100

    Added tooltip to score indicators and changed to icon instead of circle.

--- static/includeTweets.js
+++ static/includeTweets.js
@@ -1,47 +1,18 @@
 'use strict';
 
-window.onload = (function() {
-  let target = document.getElementById('result-block');
-  if (target) {
-    let ids = JSON.parse(target.getAttribute('data-tweet-ids').replace(/'/g, '"'));
-    let scores = JSON.parse(target.getAttribute('data-tweet-scores').replace(/'/g, '"'));
+$(document).ready(function() {
+    // Render tweet widgets
+    $('.tweet-container').each(function() {
+        let id = $(this).data('tweet-id');
+        twttr.widgets.createTweet(id, this, {
+            theme: 'dark',
+            width: '500px',
+            align: 'center'
+        });
+    });
 
-    if(ids.length !== scores.length) {
-      console.log('ERROR! Number of Ids does not match number of scores.')
-    }
-
-    for (let i = 0; i < ids.length; i++) {
-      let container = document.createElement('div');
-      let input = document.createElement('input');
-      let colorScale = document.createElement('div');
-      container.classList.add('select-tweet-container');
-
-      input.type = 'checkbox';
-      input.value = ids[i];
-      input.name = 'confident_tweet_id';
-      input.classList.add('select-tweet-checkbox');
-
-      colorScale.classList.add('circle--color-scale');
-      if(scores[i] > 0 && scores[i] <= 0.5) {
-        colorScale.classList.add('circle--color-scale--yellow');
-      }
-      if(scores[i] > 0.5 && scores[i] <= 0.85) {
-        colorScale.classList.add('circle--color-scale--orange');
-      }
-      if(scores[i] > 0.85 && scores[i] <= 1) {
-        colorScale.classList.add('circle--color-scale--red');
-      }
-
-      twttr.widgets.createTweet(
-        ids[i].toString(), container, {
-          theme: 'dark',
-          width: '500px',
-          align: 'center'
-        }
-      );
-      target.appendChild(container);
-      container.appendChild(input);
-      container.appendChild(colorScale)
-    }
-  }
+    // Enable tooltips
+    $('.score-indicator').tooltip({
+        placement: 'left'
+    });
 });

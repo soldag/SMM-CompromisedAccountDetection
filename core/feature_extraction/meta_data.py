e12c089ebd49cc18fdf7eef0dfc1219c9e3be733
Sören Oldag <soeren_oldag@freenet.de>
2017-03-01 16:25:16 +0100

    Fixed naming issue.

--- core/feature_extraction/meta_data.py
+++ core/feature_extraction/meta_data.py
@@ -1,15 +1,15 @@
 class MetaDataFeatures:
 
     def __init__(self, status_update):
-        self.statusUpdate = status_update
+        self.status_update = status_update
 
     def get_features(self):
-        features = [self.statusUpdate.number_of_shares,
-                    self.statusUpdate.number_of_likes,
-                    self.statusUpdate.date_time.hour,
-                    self.statusUpdate.date_time.minute,
-                    self.statusUpdate.date_time.date().weekday(),
-                    self.statusUpdate.latitude or 0,
-                    self.statusUpdate.longitude or 0]
+        features = [self.status_update.number_of_shares,
+                    self.status_update.number_of_likes,
+                    self.status_update.date_time.hour,
+                    self.status_update.date_time.minute,
+                    self.status_update.date_time.date().weekday(),
+                    self.status_update.latitude or 0,
+                    self.status_update.longitude or 0]
 
         return features
